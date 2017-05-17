5c89d4ebe9d786711dce28ab55ee250642625f00
Aleksey Rochev <alexrochevs@gmail.com>
2017-05-17 19:59:35 +0500

    Created xcode project

---
+++ BFS/BFS/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BFS
+//
+//  Created by Aleksey Rochev on 17/05/2017.
+//  Copyright © 2017 AlekseyRochev. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
